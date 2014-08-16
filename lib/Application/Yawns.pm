347c18a39a9b9779ee4d3990eefe4ebfc12cb93d
Steve Kemp <steve@steve.org.uk>
2014-08-16 08:35:04 +0100

    More work torwards login/logout.

--- lib/Application/Yawns.pm
+++ lib/Application/Yawns.pm
@@ -25,8 +25,9 @@ use HTML::Template;
 #
 # Our code
 #
+use Yawns::Event;
+use Yawns::User;
 use conf::SiteConfig;
-
 
 
 =begin doc
@@ -86,6 +87,16 @@ sub cgiapp_init
 }
 
 
+#
+#  Flush the sesions
+#
+sub teardown
+{
+    my ($self) = shift;
+
+    my $session = $self->param('session');
+    $session->flush() if ( defined($session) );
+}
 
 
 =begin doc
@@ -108,7 +119,7 @@ sub unknown_mode
 
 
 #
-#  TODO: PreRun bind to IP.
+#  TODO: PreRun IP check.
 #
 
 
@@ -131,6 +142,10 @@ sub setup
         # debug
         'debug' => 'debug',
 
+        # Login/Logout
+        'login'  => 'application_login',
+        'logout' => 'application_logout',
+
         # called on unknown mode.
         'AUTOLOAD' => 'unknown_mode',
     );
@@ -145,18 +160,6 @@ sub setup
 }
 
 
-#
-#  Handlers
-#
-sub debug
-{
-    my( $self ) = ( @_ );
-
-    my $session = $self->session();
-    my $username = $session->param( "logged_in" ) || "Anonymous";
-
-    return ("OK - $username");
-}
 
 
 #
@@ -167,6 +170,243 @@ sub my_error_rm
     my ( $self, $error ) = (@_);
 
     return Dumper( \$error );
+}
+
+
+
+=begin doc
+
+Redirect to the given URL.
+
+=end doc
+
+=cut
+
+sub redirectURL
+{
+    my ( $self, $url ) = (@_);
+
+    #
+    #  Cookie name & expiry
+    #
+    my $cookie_name   = 'CGISESSID';
+    my $cookie_expiry = '+7d';
+
+    #
+    #  Get the session identifier
+    #
+    my $query   = $self->query();
+    my $session = $self->param('session');
+
+    my $id = "";
+    $id = $session->id() if ($session);
+
+    #
+    #  Create/Get the cookie
+    #
+    my $cookie = $query->cookie( -name    => $cookie_name,
+                                 -value   => $id,
+                                 -expires => $cookie_expiry,
+                               );
+
+    $self->header_add( -location => $url,
+                       -status   => "302",
+                       -cookie   => $cookie
+                     );
+    $self->header_type('redirect');
+    return "";
+
+}
+
+
+#
+#  Real handlers
+#
+
+
+#
+#  Handlers
+#
+sub debug
+{
+    my ($self) = (@_);
+
+    my $session = $self->session();
+    my $username = $session->param("logged_in") || "Anonymous";
+
+    return ("OK - $username");
+}
+
+
+
+#
+#  Login
+#
+sub application_login
+{
+    my ($self) = (@_);
+
+    my $q       = $self->query();
+    my $session = $self->param('session');
+
+    # if already logged in redirect
+    return $self->redirectURL("/") unless ( $session->param("username") );
+
+    #
+    # If the user isn't submitting a form then show it
+    #
+    if ( $q->param("login") )
+    {
+
+        #
+        # Show the form.
+        #
+        return ("Show login form here.");
+    }
+
+
+    my $lname  = $q->param('lname');
+    my $lpass  = $q->param('lpass');
+    my $secure = $q->param('secure');
+    my $ssl    = $q->param('ssl');
+
+
+    #
+    # Login results.
+    #
+    my ( $logged_in, $suspended ) = undef;
+
+    #
+    # Do the login
+    #
+    my $user = Yawns::User->new();
+    ( $logged_in, $suspended ) =
+      $user->login( username => $lname,
+                    password => $lpass );
+
+    #
+    #  If it worked
+    #
+    if ( ($logged_in) and ( !( lc($logged_in) eq lc('Anonymous') ) ) )
+    {
+
+        my $event = Yawns::Event->new();
+        my $link  = $protocol . $ENV{ "SERVER_NAME" } . "/users/$logged_in";
+        $event->send(
+                 "Successful login for <a href=\"$link\">$logged_in</a> from " .
+                   $ENV{ 'REMOTE_ADDR' } );
+
+        #
+        #  Setup the session variables.
+        #
+        $session->param( "logged_in",    $logged_in );
+        $session->param( "failed_login", undef );
+        $session->param( "suspended",    $suspended ) if $suspended;
+
+        #
+        #  If the user wanted a secure login bind their cookie to the
+        # remote address.
+        #
+        if ( defined($secure) && ($secure) )
+        {
+            $session->param( "session_ip", $ENV{ 'REMOTE_ADDR' } );
+        }
+        else
+        {
+            $session->param( "session_ip", undef );
+        }
+
+        #
+        #  If the user wanted SSL all the time then set it up
+        #
+        if ( defined($ssl) && ($ssl) )
+        {
+            $session->param( "ssl", 1 );
+
+            #
+            # and reset the cookie to only use ssl:
+            #
+            $sessionCookie =
+              $form->cookie( -name    => 'CGISESSID',
+                             -value   => $session->id,
+                             -expires => '+1d',
+                             -secure  => 1
+                           );
+        }
+        else
+        {
+            $session->param( "ssl", undef );
+        }
+
+        #
+        # Login succeeded.  If we have a redirection target:
+        #
+        # 1:  Close session.
+        # 2:  Redirect + Set-Cookie
+        # 3:  Exit.
+        #
+        my $target = $form->param("target");
+        if ( defined($target) && ( $target =~ /^\// ) )
+        {
+            $session->close();
+            print $form->header(
+                        -type     => 'text/html',
+                        -cookie   => $sessionCookie,
+                        -location => $protocol . $ENV{ "SERVER_NAME" } . $target
+            );
+            exit;
+        }
+        else
+        {
+
+            #
+            #  No explicit target - show the homepage.
+            #
+            return ( $self->redirectURL("/") );
+        }
+    }
+    else
+    {
+
+        my $event = Yawns::Event->new();
+        $lname = "_unknown_" if ( !defined($lname) );
+        $event->send( "Failed login for $lname from " . $ENV{ 'REMOTE_ADDR' } );
+
+        #
+        # Login failed:  Invalid username or wrong password.
+        #
+        $session->param( "failed_login", 1 );
+        return ( $self->redirectURL("/login/") );
+    }
+
+}
+
+
+#
+# Logout the user.
+#
+sub application_logout
+{
+    my ($self) = (@_);
+
+    my $q       = $self->query();
+    my $session = $self->param('session');
+
+    # if already logged in redirect
+    return $self->redirectURL("/") unless ( $session->param("username") );
+
+    #
+    #  Clear the session
+    #
+    $session->param( 'logged_in', undef );
+    $session->clear('logged_in');
+    $self->param( 'session', undef );
+    $session->close();
+
+    #
+    #  Return to the homepage.
+    #
+    return ( $self->redirectURL("/") );
 }
 
 
