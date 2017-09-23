1e80ce21a372f60f2c2b4ca59c5caef001df0fbc
Steve Kemp <steve@steve.org.uk>
2017-09-23 21:56:41 +0300

    Removed redis/session/functionality

--- lib/Application/Base.pm
+++ lib/Application/Base.pm
@@ -1,14 +1,9 @@
 package Application::Base;
 use base 'CGI::Application';
 
-use CGI::Session;
-use Cache::Memcached;
-use Singleton::Redis;
-
-
 =begin doc
 
-Setup - Instantiate the session and handle cookies.
+Setup UTF-8 and nothing else.
 
 =end doc
 
@@ -16,69 +11,6 @@ Setup - Instantiate the session and handle cookies.
 
 sub cgiapp_init
 {
-    my $self  = shift;
-    my $query = $self->query();
-
-    my $cookie_name   = 'CGISESSID';
-    my $cookie_expiry = '+7d';
-    my $sid           = $query->cookie($cookie_name) || undef;
-
-    my $session;
-
-    #
-    #  Are we using memcached?
-    #
-    my $cache = conf::SiteConfig::get_conf("session");
-
-    if ( $cache =~ /^memcache:\/\/(.*)\/$/i )
-    {
-        #
-        # The host is specified after the port.
-        #
-        my $host = $1;
-
-        #
-        # Get the memcached handle.
-        #
-        my $mem = Cache::Memcached->new(
-                                         { servers => [$host],
-                                           debug   => 0
-                                         } );
-
-        # session setup
-        $session =
-          new CGI::Session( "driver:memcached", $query, { Memcached => $mem } )
-          or
-          die($CGI::Session::errstr);
-    }
-    else
-    {
-        my $redis = Singleton::Redis->new();
-        $session = CGI::Session->new( "driver:redis",
-                                      $sid,
-                                      {  Redis  => $redis,
-                                         Expire => 60 * 60 * 24
-                                      }
-          ) or
-          die($CGI::Session::errstr);
-
-    }
-
-    # assign the session object to a param
-    $self->param( session => $session );
-
-    # send a cookie if needed
-    if ( !defined $sid or $sid ne $session->id )
-    {
-        my $cookie = $query->cookie( -name     => $cookie_name,
-                                     -value    => $session->id,
-                                     -expires  => $cookie_expiry,
-                                     -httponly => 1,
-                                   );
-        $self->header_props( -cookie => $cookie );
-    }
-
-
     binmode STDIN,  ":encoding(utf8)";
     binmode STDOUT, ":encoding(utf8)";
 }
