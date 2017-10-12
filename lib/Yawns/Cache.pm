151f93a0f814b07b8cf87d44a646cae7780af5db
Steve Kemp <steve@steve.org.uk>
2017-10-12 10:03:40 +0300

    More nested directories

--- lib/Yawns/Cache.pm
+++ lib/Yawns/Cache.pm
@@ -1,3 +1,16 @@
+#
+# lib/Application/Yawns caches all content that is rendered to the
+# filesystem.
+#
+# This used to be done by writing to /tmp/$md5sum.cache, but that
+# lead to thousands of files in a single directory (/tmp).
+#
+# Now we write to /tmp/x/y/$hash
+#
+# This module takes care of testing/creating the appropriate
+# directories.
+# 
+#
 
 package Yawns::Cache;
 
@@ -24,33 +37,14 @@ sub new
     return $self;
 }
 
-sub _path
-{
-    my ( $self, $name ) = (@_);
-
-    if ( $name =~ /^(.)(.)(.*)/ )
-    {
-        my $dir  = "/tmp/$1/$2";
-        my $path = "/tmp/$1/$2/$3";
-
-        make_path( $dir,
-                   {  verbose => 0,
-                      mode    => 0755,
-                   } )
-          unless ( -d $dir );
-
-    }
-}
-
 sub exists
 {
     my ($self) = (@_);
 
     my $hash = $self->{ 'hash' };
-
-    if ( $hash =~ /^(.)(.)(.*)$/ )
+    if ( $hash =~ /^(.)(.)(.)(.)(.*)$/ )
     {
-        my $path = "/tmp/$1/$2/$3";
+        my $path = "/tmp/$1/$2/$3/$4/$5";
         return ( -e $path );
     }
     return 0;
@@ -63,11 +57,10 @@ sub path
     my ( $self, $content ) = (@_);
 
     my $hash = $self->{ 'hash' };
-
-    if ( $hash =~ /^(.)(.)(.*)$/ )
+    if ( $hash =~ /^(.)(.)(.)(.)(.*)$/ )
     {
-        my $dir  = "/tmp/$1/$2";
-        my $path = "/tmp/$1/$2/$3";
+        my $dir = "/tmp/$1/$2/$3/$4";
+        my $path= "/tmp/$1/$2/$3/$4/$5";
 
         make_path( $dir,
                    {  verbose => 0,
